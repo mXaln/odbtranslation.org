1aff717e4832c0a4166d979d1c9c3d3daa86c160
Max <runningbabushkagames@gmail.com>
2016-03-21 20:42:45 +0400

    Added PhpFastCache Library; Created Pages for translating

---
+++ www/app/Helpers/Constants/BookSources.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxim
+ * Date: 29 Feb 2016
+ * Time: 20:33
+ */
+
+namespace Helpers\Constants;
+
+
+class EventMembers
+{
+    const TRANSLATOR    = "translator";
+    const L2_CHECKER    = "checker_l2";
+    const L3_CHECKER    = "checker_l3";
+}
---
+++ www/app/views/events/translator.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxim
+ * Date: 21 Mar 2016
+ * Time: 20:24
+ */
