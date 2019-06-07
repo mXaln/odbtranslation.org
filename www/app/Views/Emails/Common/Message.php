a019873649782283165d22d127838491ab5514bf
mxaln <murmax82@gmail.com>
2019-06-07 14:32:31 -0400

    Facilitator message updated

--- www/app/Views/Emails/Common/Message.php
+++ www/app/Views/Emails/Common/Message.php
@@ -10,11 +10,11 @@ $body .= "\n\n".$data["message"];
         <meta charset="utf-8">
     </head>
     <body>
-        <div style="margin-top: 20px">
+        <div style="margin-top: 10px;">
             <div><?php echo $data["message"] ?></div>
         </div>
 
-        <div style="margin-top: 20px">
+        <div style="margin-top: 20px;">
             <div><?php echo $membersModel->translate("member_profile_message", $data["lang"]) ?>:</div>
             <div>
                 <a href="<?php echo SITEURL."members/profile/".$data["tMemberID"] ?>">
@@ -23,7 +23,7 @@ $body .= "\n\n".$data["message"];
             </div>
         </div>
 
-        <div style="color: #249b45; font-weight: bold;">
+        <div style="color: #249b45; font-weight: bold; margin-top: 20px;">
             <?php echo $membersModel->translate("facilitator_message_tip", $data["lang"]) ?>
         </div>
     </body>
